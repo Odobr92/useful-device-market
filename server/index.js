dd9a2381a0ac31d030c74274b53704a8b96af958
odobryaev_a <odobr92@gmail.com>
2022-08-20 21:04:24 +0300

    Добавлен Middelware для обработки ошибок

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const model = require('./models/models');
 const app = express();
 const cors = require('cors');
 const router = require('./routes/index');
+const errorMiddelware = require('./middleware/ErrorHandlingMiddleware');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+app.use(errorMiddelware); //Используем в конце
 
 const start = async () => {
     try {
