08e0344599628b9bc0ee159d9fb0daca327a798a
odobryaev_a <odobr92@gmail.com>
2022-08-20 19:03:25 +0300

    Добавлены роуты для дальнейших api-команд

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const sequelize = require('./db')
 const PORT = process.env.PORT || 5000;
 const model = require('./models/models');
 const app = express();
+const cors = require('cors');
+const router = require('./routes/index');
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
     try {
