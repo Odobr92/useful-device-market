0bbf5ee7ff9236413cf11ba4e82da11e314af050
odobryaev_a <odobr92@gmail.com>
2022-08-21 19:07:33 +0300

    Реализация для контроллеров, регистрация статических файлов и подключены пакеты
    Полная реализация для brand и type
    Для device только создание устройства
    
    Подключены библиотеки:
     uuid - для генерации названия картинок
     express-fileupload - для загрузки картинок

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const sequelize = require('./db')
 const PORT = process.env.PORT || 5000;
 const model = require('./models/models');
@@ -7,9 +8,12 @@ const app = express();
 const cors = require('cors');
 const router = require('./routes/index');
 const errorMiddelware = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 app.use(errorMiddelware); //Используем в конце
