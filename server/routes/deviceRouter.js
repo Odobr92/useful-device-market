d13084b7dd22cef013933258245f5945a092a8de
odobryaev_a <odobr92@gmail.com>
2022-08-21 19:00:02 +0300

    Добавлена привязка контроллеров к роутерам (для остальных сущностей)

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const router = new Router();
+const DeviceRouter = require('../controllers/deviceController');
 
-router.post('/',);
-router.get('/',);
-router.get('/:id',);
+router.post('/', DeviceRouter.create);
+router.get('/', DeviceRouter.getAll);
+router.get('/:id', DeviceRouter.getOne);
 
 module.exports = router;
